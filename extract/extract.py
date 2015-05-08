533cdde328ed547745ec4bd0b0f4471535d7c387
Rene Knaebel <rknaebel@uni-potsdam.de>
2015-05-08 12:56:42 +0200

    Fixed python script: ger,eng options

--- extract/extract.py
+++ extract/extract.py
@@ -12,15 +12,19 @@ import sys
 
 # Main
 def main():
-    if len(sys.argv) != 2:
-        print "Usage: extract.py <src>"
+    if len(sys.argv) != 3:
+        print "Usage: extract.py <lang>[ger|eng] <src>"
         return 1
-    srcFile = sys.argv[1]
+    
+    srcFile = sys.argv[2]
     with open(srcFile, "r") as fh:
         matches = re.findall("([+0-9]*)'\s*([^\n]*)", fh.read())
         matches.sort(key=lambda x: eval(x[0]))
         for m in matches:
-            mm = re.sub(r"([0-9]+):([0-9]+)",r"\1 zu \2",m[1])
+            if sys.argv[1] == "ger":
+                mm = re.sub(r"([0-9]+):([0-9]+)",r"\1 zu \2",m[1])
+            elif sys.argv[1] == "eng":
+                mm = re.sub(r"([0-9]+):([0-9]+)",r"\1 to \2",m[1])
             mm = mm.replace(". ", ".\n")
             mm = mm.replace("! ", "!\n")
             mm = mm.replace("? ", "?\n")
