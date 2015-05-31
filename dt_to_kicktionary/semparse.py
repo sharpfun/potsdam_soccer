e9363b2925795ecb9e3b32e6377ca2f0fbb2fdee
kaiserklayton <claytonvioland@gmail.com>
2015-05-31 18:17:24 +0200

    added verbnet.py

--- dt_to_kicktionary/semparse.py
+++ dt_to_kicktionary/semparse.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
-# Main script for identifying Kicktionary (and Verbnet) frames for ticker sentences
-# 1. First look up only roots in Kicktionary directly
-# 2. Then look up roots and objects
-# 3. Then look up roots in Verbnet
-# 4. With Verbnet frames, look up in Kicktionary again
+# Main script, identifies Kicktionary and Verbnet frames for ticker sentences.
+# 1. Look up roots in Kicktionary.
+# 2. Look up root-object pairs in Kicktionary.
+# 3. Look up roots in Verbnet.
+# 4. Look up Verbnet frame-members in Kicktionary, recursively.
+# 5. Last Resort: If no frame-member match is found, use (3) as match.
 
 from __future__ import division
+import collections
+import copy
+import optparse
+import xml.sax
+
 from bs4 import BeautifulSoup
-import nltk, copy, collections, xml.sax
+import nltk
+
 from kicktionary import *
 from ticker import *
 from verbnet import *
@@ -21,19 +28,37 @@ def kicktionary_lookup(kicktionary, ticker, verbose):
             if tree.root == lu.lemma:
                 tree.lexical_unit = lu
                 if verbose: print "Tree " + str(tree.tree_id) + " root matches Kicktionary lexical unit: " + tree.lexical_unit.lemma
-    ## TODO: find when the tree root matches more than one Kicktionary lexical unit
-    ## and somehow disambiguate
+    # TODO: find when the tree root matches more than one Kicktionary lexical unit and somehow disambiguate.
     
     return ticker
-               
+
+# Added Root-->Verbnet.Frame.Member-->Kicktionary LU Lookup, and Root-->Verbnet lookup.
+#def verbnet_lookup(verbnet, ticker, verbose):
+#    if verbose: print "Looking up tree roots in Verbnet..."
+#
+#    for tree in ticker:
+#        for vb in verbnet:
+#            member_match = _frame_member_lookup(vb, kicktionary)
+#            if member_match != None:
+#                tree.lexical_unit = member_match
+#                if verbose: print "Tree " + str(tree.tree_id) + " root (via verbnet frame-member relation) matches Kicktionary lexical unit: " + tree.lexical_unit.lemma
+#            elif tree.root == vb.lemma:
+#                tree.lexical_unit = vb
+#                if verbose: print "Tree " + str(tree.tree_id) + " root matches Verbnet lexical unit: " + tree.lexical_unit.lemma
+
+#    return ticker
+
+#def _frame_member_lookup(vb, kicktionary):
+#    if verbose: print "Looking up Verbnet frame-members in Kicktionary..."
+#    for fm in 
+
 def main():
-    import optparse
     parser = optparse.OptionParser()
     parser.add_option("--kicktionary", dest="kicktionary", help="location of kicktionary xml file", default="../data/kicktionary.xml")
     parser.add_option("--ticker", dest="ticker", help="location of parsed ticker conll file", default="../data/p2.parsed")
     parser.add_option("--verbose", dest="verbose", help="print helpful messages about the progress", default=False)
     parser.add_option("--language", dest="language", help="language of tickers (en or de)", default="en")
-    ## will need to add verbnet XML file(s) at some point, maybe a complete folder
+    ## will need to add verbnet XML file(s) at some point, maybe a complete folder.
     parser.add_option("--verbnet", dest="verbnet", help="location of folder with verbnet xml files", default="../data/verbnet")
     (options, args) = parser.parse_args()
 
@@ -42,10 +67,10 @@ def main():
         
     kicktionary = read_kicktionary(options.kicktionary, verbose, language)
     ticker = read_ticker(options.ticker, verbose, language)
-    # verbnet = read_verbnet(options.verbnet)
+    verbnet = read_verbnet(options.verbnet, verbose, language)
 
     ticker_with_lus = kicktionary_lookup(kicktionary, ticker, verbose)
+    # TODO: Add ticker_with_verbs.
 
 if __name__ == "__main__":
     main()
-
--- dt_to_kicktionary/verbnet.py
+++ dt_to_kicktionary/verbnet.py
@@ -1,3 +1,46 @@
 #!/usr/bin/env python
 
-# Reads in the verbnet xml files and returns a list of verbnet objects
+# Defines Verb class and read_verbnet() function.
+# Reads in Verbnet xml files and returns list of Verb objects.
+
+import os
+
+from bs4 import BeautifulSoup
+
+
+# TODO: Add in other useful attributes from Verbnet .xml files.
+class Verb(object):
+
+    def __init__(self):
+        self.lemma = None
+        self.frame = None
+
+
+# TODO: read in possible arguments for each lexical unit (?).      
+class Argument(object):
+
+    def __init__(self):
+        self.name = None
+        self.type = None
+
+
+def read_verbnet(verbnet_folder, verbose, language):
+    if verbose: print "Reading Verbnet..."
+   
+    verbnet = []
+    for filename in os.listdir(verbnet_folder):
+        if verbose: print "Loading Verb net file " + filename
+        soup = BeautifulSoup(open(verbnet_folder + "/" + filename), "xml")
+        verb = soup("MEMBER")
+        frame = soup("VNCLASS")
+   
+        for item in verb:
+            vb = Verb()
+            vb.lemma = item["name"]
+            vb.frame = frame["ID"]
+            print "%s + (%s)" % vb.lemma, vb.frame
+            verbnet.append(vb)     
+  
+    if verbose: print "Read in " + str(len(verbnet)) + " verbnet verbs"   
+       
+    return verbnet
