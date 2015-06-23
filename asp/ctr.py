5c4f1331caf0359f39e138584e27490417ac27b5
Rene Knaebel <reneknaebel90@gmail.com>
2015-06-23 18:37:09 +0100

    removed negative fluents in states still have problems substituting ramsey for clayton...

--- asp/ctr.py
+++ asp/ctr.py
@@ -7,23 +7,29 @@ def onModel(model):
     atoms = model.atoms(Model.ATOMS)
     events = getFluents(atoms, "ticker")
     states = getFluents(atoms, "holds" )
+    attributes = getFluents(atoms, "attribute" , time=False)
     for time in range(len(states)):
         print "{}".format(time)
         if time in events:
             for e in events[time]:
                 print "[e]  {}".format(e)
+                for att in attributes:
+                    if att[0] == e: print "---  {}: {}".format(att[1], att[2])
         print "[s] "
         for s in states[time]:
             print "     {}".format(s)
 
-def getFluents(atoms, name):
-    fluents = {}
-    for (lit,time) in [atom.args() for atom in atoms if atom.name() == name]:
-        if time not in fluents:
-            fluents[time] = [lit]
-        else:
-            fluents[time].append(lit)
-    return fluents
+def getFluents(atoms, name, time=True):
+    if time:
+        fluents = {}
+        for (lit,time) in [atom.args() for atom in atoms if atom.name() == name]:
+            if time not in fluents:
+                fluents[time] = [lit]
+            else:
+                fluents[time].append(lit)
+        return fluents
+    else:
+        return [atom.args() for atom in atoms if atom.name() == name]
 
 def main(ctr):
     ctr.ground([("base", [])])
