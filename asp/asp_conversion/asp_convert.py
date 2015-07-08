bedea466da00a56226b39e68daff66b826b1d2f2
kaiserklayton <claytonvioland@gmail.com>
2015-07-08 11:13:06 +0200

    modularized asp conversion functionality

--- asp/asp_conversion/asp_convert.py
+++ asp/asp_conversion/asp_convert.py
@@ -143,17 +143,107 @@ def find_arguments(ticker, verbose):
     # return a list of events with arguments identified
     return events
 
+##################################################
+
+def to_asp(events, write=True):
+    # TODO: import metadata lists ...
+    # Question: What values do we want as integers, and not strings???
+        # Minute? 
+        # Ticker_ID? 
+        # Event_ID?
+    team_one = "arsenal"
+    team_two = "hull city"
+    playerdata_one = [('A','Red'),('B','Red'),('C','Red'),('D','Red'),('E','Red'),('F','Red'),('G','Red'),('H','Red'),('I','Red'),('J','Red'),('K','Red')]
+    benchdata_one = [('L','Red'),('M','Red'),('N','Red'),('O','Red'),('P','Red'),('Q','Red'),('R','Red'),('S','Red'),('T','Red')]
+    playerdata_two = [('AA','Blue'),('BB','Blue'),('CC','Blue'),('DD','Blue'),('EE','Blue'),('FF','Blue'),('GG','Blue'),('HH','Blue'),('II','Blue'),('JJ','Blue'),('KK','Blue')]
+    benchdata_two = [('LL','Blue'),('MM','Blue'),('NN','Blue'),('OO','Blue'),('PP','Blue'),('QQ','Blue'),('RR','Blue'),('SS','Blue'),('TT','Blue')]
+    
+    asp = []
+
+    ## META SECTION ##
+    # Match Statistics Initialization.
+    asp.extend((    '#const maxScore=20.',
+                    'score(0..maxScore).',
+                    'time(0).'
+                    ))
+
+    # Teams --> FROM METADATA:
+    asp.extend((    'team(%s, 1).' % team_one,
+                    'team(%s, 2).' % team_two
+                    ))
+
+    # Players & Their Teams: Team One --> FROM METADATA
+    for p,t in playerdata_one:
+        asp.append( 'player(%s,%s).' % (p,t) )
+
+    for b,t in benchdata_one:
+        asp.append( 'bench(%s,%s).' % (b,t) )
+
+    # Players & Their Teams: Team Two --> FROM METADATA
+    for p,t in playerdata_two:
+        asp.append( 'player(%s,%s).' % (p,t) ) 
+
+    for b,t in benchdata_two:
+        asp.append( 'bench(%s,%s).' % (b,t) )
+
+    ## FOR EASE OF USE DOWN THE ROAD... ##
+    asp.extend((    'player(P) :- player(P,T).',
+                    'team(T)   :- player(P,T).',
+                    'bench(P)  :- bench(P,T).'
+                    ))
+
+    ## FLUENTS ##
+    asp.extend((    'fluent(score(S,T)) :- score(S), team(T).',
+                    'fluent(ball(P))    :- player(P).',
+                    'fluent(player(P))  :- player(P).',
+                    'fluent(player(P))  :- bench(P).',
+                    'fluent(bench(P))   :- player(P).',
+                    'fluent(bench(P))   :- bench(P).'
+                    ))
+
+    ## INITS ##
+    asp.extend((    'init(score(0,T))    :- team(T).',
+                    'init(player(P))     :- player(P).',
+                    'init(bench(P))      :- bench(P).',
+                    'init(neg(ball(P)))  :- player(P).'
+                    ))
+
+    ###########################
+
+    ## DYNAMIC ACTION!!!
+    for i in events:
+        a = i.ticker
+        b = i.minute
+        c = i.text.split()
+        d = i.arguments
+        e = i.event_id
+        f = i.frame
+        asp.append( 'ticker(%s,%s,%s,%s).' % (e,b,f.lower(),a) )
+
+        for j in i.arguments:
+            asp.append( 'attribute(%s,%s,%s,%s,%s).' % (e,b,f.lower(),j.lower(),i.arguments[j].lower()) )
+
+    if write == True:
+        f = open('../game_instance.lp','w+')
+        for item in asp:
+            f.write(item + '\n')
+        f.close()
+
+    return asp
+
+##################################################
+
 # for testing
 def main():
     import optparse
     parser = optparse.OptionParser()
-    parser.add_option("--kicktionary", dest="kicktionary", help="location of kicktionary xml file", default="../data/kicktionary.xml")
-    parser.add_option("--ticker", dest="ticker", help="location of parsed ticker conll file", default="../data/p4.parsed")
+    parser.add_option("--kicktionary", dest="kicktionary", help="location of kicktionary xml file", default="../../data/kicktionary.xml")
+    parser.add_option("--ticker", dest="ticker", help="location of parsed ticker conll file", default="../../data/p4.parsed")
     parser.add_option("--verbose", dest="verbose", help="print helpful messages about the progress", default=False)
     parser.add_option("--language", dest="language", help="language of tickers (en or de)", default="en")
     ## will need to add verbnet XML file(s) at some point, maybe a complete folder
-    parser.add_option("--verbnet", dest="verbnet", help="location of folder with verbnet xml files", default="../data/verbnet")
-    parser.add_option("--luorder", dest="luorder", help="location of folder with lexical unit order file", default="../data/luorder")
+    parser.add_option("--verbnet", dest="verbnet", help="location of folder with verbnet xml files", default="../../data/verbnet")
+    parser.add_option("--luorder", dest="luorder", help="location of folder with lexical unit order file", default="../../data/luorder")
     (options, args) = parser.parse_args()
 
     verbose = options.verbose
@@ -167,14 +257,21 @@ def main():
  #   ticker_with_lus = kicktionary_lookup_possible_lu(kicktionary, ticker, verbose, luorder)
     
     events = find_arguments(ticker, verbose)
+
+##################################################
+    ## TESTING THE ASP CONVERSION FUNCTIONALITY ##
+    asp = to_asp(events)
+    for i in asp:
+        print i
+##################################################
     
-    for event in events:
-        if event.frame != None and event.frame != "":
-            print event.ticker
-            print event.minute
-            print event.text
-            print event.arguments
-            print ""
+ #   for event in events:
+ #       if event.frame != None and event.frame != "":
+ #           print event.ticker
+ #           print event.minute
+ #           print event.text
+ #           print event.arguments
+ #           print ""
 
 if __name__ == "__main__":
     main()
